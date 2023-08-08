eb0d945db49e40ebb626359cadb37c4c6ad6c296
kong33 <kge021114@naver.com>
2023-08-08 18:47:00 +0900

    중간 업데이트 확인 불필요

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return(
         <header>
-
+            <h1 className= "header_text1">HEAVENLY</h1>
+            <h1 className= " header_text2">COCKTAIL</h1>
         </header>
     )
 }
---
+++ src/page/StartPage_1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LayoutOnlyHeader from '../Layout/Layout/LayoutOnlyHeader';
+import Header from '../Layout/Header/Header';
+
+const StartPage_1 = () =>{
+    return(
+     <Header/>
+    )
+}
