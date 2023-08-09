2e5e333275c06d36373b4b9667f6e689ffc032a0
kong33 <kge021114@naver.com>
2023-08-09 20:31:26 +0900

    create button component
    button 컴포넌트를 작성하고 pages/startpage_1에 사용 예시 작성해둠. 서버가 안열려서 확인 못했으며 storybook 에러로 작성 못 함. 만나서 한 번 세팅 손봐야 할듯

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,28 +1,18 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-        // For more details on this addon's options.
-        postCss: {
-          implementation: require.resolve('postcss'),
-        },
-      },
-    },
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
 };
 export default config;
--- src/pages/StartPage_1.jsx
+++ src/pages/StartPage_1.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import LayoutOnlyHeader from '../components/Layout/Layout/LayoutOnlyHeader';
 import Header from '../components/Layout/Header/Header';
-
+import button from '../images/버튼예시.png';
 const StartPage_1 = () =>{
     return(<div>
      <Header/>
      <div>
-        <img src=""></img>
+     <View style={styles.container}>
+      <ImageButton
+        imageUrl={button} // 로컬 이미지 파일 경로
+        linkUrl=""
+      />
+    </View>
     </div>
     
      </div>
