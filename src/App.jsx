f7e46cdfee1eda0c40ebd53ba2ba05a9da50060d
kong33 <kge021114@naver.com>
2023-08-19 01:33:17 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import StartPage_1 from './pages/StartPage_1.jsx';
-
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import StartPage_2 from './pages/StartPage_2.jsx';
 function App() {
 
   return (<div>
+  <Router>
+    <Link to="/"></Link>
+    <Link to="/other"></Link>
+
+    <Switch>
+      <Route path="/other">
+        <StartPage_2/>
+      </Route>
+      <Route path="/">
+      </Route>
+    </Switch>
+
+  </Router>
     <StartPage_1/>
     </div>
   )
--- src/pages/StartPage_2.jsx
+++ src/pages/StartPage_2.jsx
@@ -1,4 +1,26 @@
 import React, {useState} from 'react';
 import './StartPage_2.css';
 import font from '../font/NotoSansKR-Regular.otf';
+import header from '../images/headerImage.png';
+import {Link} from 'react-router-dom';
+const StartPage_2=()=>{
+    return(
+        <div>
+            <img className="headerIMG" src={header}/>
+            <p className="littlewords">YOU ARE</p>
+            <p className="bigwords">MICHELANGELO</p>
+            <p className='lowerwords'>
+            당신이 머무는 공간에서는 어떤 색의 향이 흐르나요<br/>
+            디퓨저 조향 질문지를 통해서 당신이<br/>
+            마음껏 취할 수 있는 천국의 향을 만들어 보세요</p>
+            <button className="start">
+                <Link to="/other">조향 시작하기</Link>
+                <Link to="/other">추천받기</Link>
+            </button>
 
+
+        </div>
+    )
+}
+
+export default StartPage_2;
