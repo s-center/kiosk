7bcfdc95c4381e944dbbd584b7e673f5e4c9573f
ENvironmentSet <sbshw1@gmail.com>
2023-08-29 15:58:47 +0900

    Add ViewBox
    키오스크와 실제 개발 환경의 화면비가 다른 점을 해결하기 위해 키오스크와 같은 비율의 렌더 공간을 만들어 주는 컴포넌트입니다.

---
+++ src/ViewBox.jsx
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+export const ViewBox = ({ children }) => (
+  <div
+    css={css`
+      width: 957px;
+      height: 1736px;
+      
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    `}
+  >
+    {children}
+  </div>
+)
