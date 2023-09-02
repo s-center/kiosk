883d665eebcd16cb1fbadda5d04a3bff2ddefb62
ENvironmentSet <sbshw1@gmail.com>
2023-09-02 15:22:09 +0900

    format eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,8 +21,8 @@ module.exports = {
       allowConstantExport: true
     }],
     'react/prop-types': 0,
-    "react/no-unknown-property": ['error', { ignore: ['css'] }],
-    'indent': ["error", 2],
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'indent': ['error', 2],
     'semi': ['error', 'never']
   }
 };
