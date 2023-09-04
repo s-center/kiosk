49a38d5cdc0936e87ea92fa944b777a10e6ac995
kong33 <kge021114@naver.com>
2023-09-04 15:44:09 +0900

    ./

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Redirect, Route, Switch } from 'wouter';
+import { Starting } from './pages/Starting';
+import { Choosing } from './pages/Choosing';
+export const App = () => (
+  <Switch>
+    <Route exact path='/starting'> <Starting /> </Route>
+    <Route path='/choosing'><Choosing /></Route>
+    <Route><Redirect to='/starting' /> {/** Or should we redirect to 404 page? */}</Route>
+    
+  </Switch>
+)
