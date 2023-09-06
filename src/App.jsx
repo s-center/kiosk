b34a90a1ebf0416f6d39f133a9889b36dd14ddbb
kong33 <96658105+kong33@users.noreply.github.com>
2023-09-06 13:57:10 +0900

    Update App.jsx
    피드백 반영-start page path 변경

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Starting } from './pages/Starting'
 import { Choosing } from './pages/Choosing'
 export const App = () => (
   <Switch>
-    <Route exact path='/starting'> <Starting /> </Route>
+    <Route exact path='/'> <Starting /> </Route>
     <Route path='/choosing'><Choosing /></Route>
-    <Route><Redirect to='/starting' /> {/** Or should we redirect to 404 page? */}</Route>
+    <Route><Redirect to='/' /> {/** Or should we redirect to 404 page? */}</Route>
     
   </Switch>
 )
