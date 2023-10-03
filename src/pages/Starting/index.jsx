ed9bf7c79fa8fe6a26376cd31fbbc76fbbacd793
ENvironmentSet <sbshw1@gmail.com>
2023-10-03 16:08:31 +0900

    Remove unused import

--- src/pages/Starting/index.jsx
+++ src/pages/Starting/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { navigate } from 'wouter/use-location'
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
