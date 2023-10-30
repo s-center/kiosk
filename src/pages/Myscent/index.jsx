72cc44e5fc2244ca1f0c7b457933fd7c86238fdd
kong33 <kge021114@naver.com>
2023-10-30 22:13:21 +0900

    myscent

---
+++ src/pages/Myscent/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Logo } from '../../components/Logo/index'
+
+export const Myscent = () => {
+    return(
+        <div>
+            <img src='' alt="배경이미지"></img>
+            <Logo className ="myscentLogo"/>
+            <img src='' alt="bottle"></img>
+            
+        </div>
+
+    )
+
+}
