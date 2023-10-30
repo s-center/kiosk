295dff0c4b53f77f0439398dad61d66418429aee
ENvironmentSet <sbshw1@gmail.com>
2023-10-30 20:54:31 +0900

    Implement place selection page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Redirect, Route, Switch } from 'wouter'
 import { Starting } from './pages/Starting'
 import { Choosing } from './pages/Choosing'
-
 import { scentChoosing } from './pages/Scentchoosing'
+import { PlaceChoosing } from './pages/PlaceChoosing'
 
 export const App = () => (
   <Switch>
     <Route exact path='/starting' component={ Starting }/>
     <Route path='/choosing' component={ Choosing }/>
     <Route path ='/scentchoosing' component={ scentChoosing }/>
+    <Route path='/placechoosing' component={ PlaceChoosing } />
     <Route><Redirect to='/starting' /> {/** Or should we redirect to 404 page? */}</Route>
 
-    
+
   </Switch>
 )
