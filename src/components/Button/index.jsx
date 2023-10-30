4c785e644735d292afa6cc4d32d3af6807712812
kong33 <kge021114@naver.com>
2023-10-30 14:49:47 +0900

    중간 업로드

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,8 +4,8 @@ import { css } from '@emotion/react'
 export const Button = ({ buttonText, to, className }) => {
 
   const buttonStyles = css`
-    background-color: '#000'; 
-    opacity: 0.5;
+    background-color: gray; 
+    opacity: 1;
     display: flex;
     width: 648px;
     height: 132px;
@@ -20,7 +20,7 @@ export const Button = ({ buttonText, to, className }) => {
 
   return (
     <button css={[ buttonStyles, className ]}>
-      <Link to = { to } css ={ css`color:#FFF; text-decoration: none; `}>{ buttonText }</Link>
+      <Link to = { to } css ={ css`color: white; text-decoration: none; `}>{ buttonText }</Link>
     </button>
   )
 }
