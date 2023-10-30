651480646c88cb77b899191165e44ac370d9ae4a
ENvironmentSet <sbshw1@gmail.com>
2023-10-30 17:23:45 +0900

    Automatic fix via eslint

--- src/pages/Scentchoosing/index.jsx
+++ src/pages/Scentchoosing/index.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import { Layout } from '../Layout/index'
 import { css } from '@emotion/react'
 export const scentChoosing = () => {
-    return (
-        <div css = {css`
+  return (
+    <div css = {css`
             width: 100%;
             height: 100%;`}>
-            <Layout downBanner=""> <div><p css={css`color: white;`}>더미디브</p></div></Layout>
-        </div>
+      <Layout downBanner=""> <div><p css={css`color: white;`}>더미디브</p></div></Layout>
+    </div>
 
-    )
+  )
 }
