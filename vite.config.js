f5d282def4322030272496cf9d1d1893c8fb8779
ENvironmentSet <sbshw1@gmail.com>
2023-11-08 18:09:46 +0900

    Revert "add million.js"
    This reverts commit 145e77105b08d7aa2ea14d8a34dedde4871a6e40.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import million from 'million/compiler';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), 
+  plugins: [
     react({
       jsxImportSource: '@emotion/react',
       babel: {
