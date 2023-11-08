a7c9749de8238dc0d24d0dc93c1cc459fdf8a28c
kong33 <kge021114@naver.com>
2023-11-08 16:33:03 +0900

    ,

--- src/pages/QRcode/index.jsx
+++ src/pages/QRcode/index.jsx
@@ -25,10 +25,10 @@ export const QRcode = () => {
             font-size: 250%;
             font-family: NotoSansKR-Medium;`}>조향사에게 보여주세요</p>
             <div css={css`position: absolute; top: 75%; left: 20%;`}>
-            <Button to='/recommand' buttonText={'시제품 추천받기'} className={css`background-color:rgb(255,255,255);`} 
-            linkStyle={css`
-            padding:30px; color: rgb(0,0,0);`}/>
-            <Button to='/starting' buttonText={'홈으로 돌아가기'} className={css`background-color:rgb(0,0,0);`}/>
+                <Button to='/recommand' buttonText={'시제품 추천받기'} className={css`background-color:rgb(255,255,255);`}
+                    linkStyle={css`
+            padding:30px; color: rgb(0,0,0);`} />
+                <Button to='/starting' buttonText={'홈으로 돌아가기'} className={css`background-color:rgb(0,0,0);`} />
             </div>
         </div>
     )
