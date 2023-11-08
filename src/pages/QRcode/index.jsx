68ea5aead0f450da72a0d0cec96ec21051a61d2c
ENvironmentSet <sbshw1@gmail.com>
2023-11-08 16:42:15 +0900

    fix file path

--- src/pages/QRcode/index.jsx
+++ src/pages/QRcode/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import QRimage from '../../assets/qrcode.png'
+import QRimage from '../../assets/QRCODE.png'
 import { Logo } from '../../components/Logo'
 import { css } from '@emotion/react'
 import qrBackground from '../../assets/QRimage/qrBackground.png'
 import { Button } from '../../components/Button'
-import QRCODE from '../../assets/Qrtext.png'
+import QRCODE from '../../assets/QRtext.png'
 export const QRcode = () => {
 
   return (
@@ -25,7 +25,7 @@ export const QRcode = () => {
             font-size: 250%;
             font-family: NotoSansKR-Medium;`}>조향사에게 보여주세요</p>
       <div css={css`position: absolute; top: 75%; left: 20%;`}>
-        <Button to='/recommand' buttonText={'시제품 추천받기'} className={css`background-color:rgb(255,255,255);`} 
+        <Button to='/recommand' buttonText={'시제품 추천받기'} className={css`background-color:rgb(255,255,255);`}
           linkStyle={css`
             padding:30px; color: rgb(0,0,0);`}/>
         <Button to='/starting' buttonText={'홈으로 돌아가기'} className={css`background-color:rgb(0,0,0);`}/>
