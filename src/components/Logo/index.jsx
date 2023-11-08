2f7cf0cdd3e18c206fb6da86d9c7f3d06c6b58e9
ENvironmentSet <sbshw1@gmail.com>
2023-11-08 17:23:48 +0900

    add missing navigator

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,7 +1,7 @@
 import logo from '../../assets/logo.png'
 import { Link } from 'wouter'
 export const Logo = ({ className }) => (
-  <Link to ='/'>
+  <Link to='/'>
     <img className={className} src={logo} alt='Heavenly cocktail logo'/>
   </Link>
 )
